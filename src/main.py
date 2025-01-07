0d82e84903abf3e16ec83cd79f9520e4595a2eef
kafkapple <biasdrive@gmail.com>
2025-01-07 13:57:46 +0900

    fix: src module import path

--- src/main.py
+++ src/main.py
@@ -1,4 +1,10 @@
 import os
+import sys
+
+# 프로젝트 루트 디렉토리를 Python 경로에 추가
+project_root = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+sys.path.append(project_root)
+
 import hydra
 import warnings
 import torchvision
